bf228e0c759c0a6e45ffa7af2ac2af3b98048854
pu19249 <pu19249@uvg.edu.gt>
2023-06-09 20:11:18 -0600

    Tab update
    Window methods and tab in main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
+'''
+frontend
+'''
+import tkinter as tk
+from tkinter import ttk
+from windows.simulation_tab import Window
 
+# Main window definition
+main_window = tk.Tk()
+main_window.title("Robotat GUI")
+main_window.geometry("800x500")
+main_window.resizable(0, 0)
+
+# Tabs definition
+notebook = ttk.Notebook(main_window)
+
+sim_tab = Window(notebook)
+notebook.add(sim_tab, text="Simulator")
+
+ota_tab = Window(notebook)
+notebook.add(ota_tab, text="OTA")
+
+rtd_tab = Window(notebook)
+notebook.add(rtd_tab, text="RTD")
+
+notebook.pack(expand=True, fill="both")
+
+main_window.mainloop()
--- src/windows/simulation_tab.py
+++ src/windows/simulation_tab.py
@@ -1,1 +1,60 @@
+# Class definition for each of the tabs in the GUI
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from datetime import datetime 
+'''
+Each window shall have:
+- a CLOSE btn
+- date and other basic information in the upper part
+- message console
+'''
+# class Window(ttk.Frame):
+#     def __init__(self, master):
+#         super().__init__(master)
+        
 
+#         # close
+#         self.btn = ttk.Button(self, text="Close", command=self.close_window)
+#         self.btn.place()
+
+#         # date
+#         self.today_info = tk.StringVar()
+#         self.update_date()
+#         self.today_info_disp = ttk.Label(self, textvariable=self.today_info)
+#         self.today_info_disp.place()
+
+#         self.pack()
+
+#     def close_window(self):
+#         self.master.destroy()
+
+#     def update_date(self):
+#         current_date = datetime.now().strftime("%A, %B %d, %Y")
+#         self.today_info.set(current_date)
+#         self.after(1000, self.update_date)  # Update the date every second (1000 milliseconds)
+
+class Window(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+
+        # close
+        self.btn = ttk.Button(self, text="Close", command=self.close_window)
+        self.btn.pack(side=tk.TOP)
+
+        # date
+        self.today_info = tk.StringVar()
+        self.update_date()
+        self.today_info_disp = ttk.Label(self, textvariable=self.today_info)
+        self.today_info_disp.pack(side=tk.TOP)
+
+        self.pack(expand=True, fill="both")
+
+    def close_window(self):
+        self.master.destroy()
+
+    def update_date(self):
+        current_date = datetime.now().strftime("%A, %B %d, %Y")
+        self.today_info.set(current_date)
+        self.after(1000, self.update_date)
