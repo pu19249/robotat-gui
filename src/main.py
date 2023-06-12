2a6eed0b40644052c1f2ed097c8fe08f7e081562
pu19249 <pu19249@uvg.edu.gt>
2023-06-12 14:26:20 -0600

    Console and error method

--- src/main.py
+++ src/main.py
@@ -15,6 +15,12 @@ main_window.resizable(0, 0)
 notebook = ttk.Notebook(main_window)
 
 sim_tab = Window(notebook)
+dictionary_sim = {
+    "er1" : "Test",
+    "er2" : "Test2"
+}
+sim_tab.set_error_list(dictionary_sim)
+sim_tab.display_error("er1")
 notebook.add(sim_tab, text="Simulator")
 
 ota_tab = Window(notebook)
@@ -23,6 +29,29 @@ notebook.add(ota_tab, text="OTA")
 rtd_tab = Window(notebook)
 notebook.add(rtd_tab, text="RTD")
 
+
 notebook.pack(expand=True, fill="both")
 
 main_window.mainloop()
+
+
+  
+# lbl = Label(app,text = "A list of favourite countries...")  
+# lbl.pack() 
+# listbox = Listbox(app)  
+# listbox.pack()  
+
+
+# listbox.insert(1,"text 1")  
+  
+# listbox.insert(2, "text 2")  
+  
+# listbox.insert(3, "text 3")  
+  
+# listbox.insert(4, "text 4")  
+  
+ 
+
+  
+# app.mainloop()  
+
--- src/windows/simulation_tab.py
+++ src/windows/simulation_tab.py
@@ -9,30 +9,6 @@ Each window shall have:
 - date and other basic information in the upper part
 - message console
 '''
-# class Window(ttk.Frame):
-#     def __init__(self, master):
-#         super().__init__(master)
-        
-
-#         # close
-#         self.btn = ttk.Button(self, text="Close", command=self.close_window)
-#         self.btn.place()
-
-#         # date
-#         self.today_info = tk.StringVar()
-#         self.update_date()
-#         self.today_info_disp = ttk.Label(self, textvariable=self.today_info)
-#         self.today_info_disp.place()
-
-#         self.pack()
-
-#     def close_window(self):
-#         self.master.destroy()
-
-#     def update_date(self):
-#         current_date = datetime.now().strftime("%A, %B %d, %Y")
-#         self.today_info.set(current_date)
-#         self.after(1000, self.update_date)  # Update the date every second (1000 milliseconds)
 
 class Window(ttk.Frame):
     def __init__(self, master):
@@ -49,12 +25,34 @@ class Window(ttk.Frame):
         self.today_info_disp = ttk.Label(self, textvariable=self.today_info)
         self.today_info_disp.pack(side=tk.TOP)
 
+        # console
+        self.console = tk.Text(self, height=4)
+        self.console.pack(fill="x")
+        # initial message
+        self.console.insert("end", "Robotat GUI console")
+        # disable input option
+        self.console.config(state=DISABLED) 
+
+        # dictionary will change (with method set_error_list) depending on the tab
+        self.error_dictionary = {
+            "error1":"Error 1"
+        }
+
         self.pack(expand=True, fill="both")
 
     def close_window(self):
-        self.master.destroy()
+        self.master.master.destroy()
 
     def update_date(self):
         current_date = datetime.now().strftime("%A, %B %d, %Y")
         self.today_info.set(current_date)
         self.after(1000, self.update_date)
+
+    def display_error(self, error_type):
+        # enable before displaying the next message
+        self.console.config(state=NORMAL)
+        self.console.insert("end", self.error_dictionary[error_type])
+        self.console.config(state=DISABLED)
+
+    def set_error_list(self, error_list):
+        self.error_dictionary = error_list
