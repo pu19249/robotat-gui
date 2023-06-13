b71f32f55d10a2262f26464205ecc8d3a3335b81
pu19249 <pu19249@uvg.edu.gt>
2023-06-13 12:57:44 -0600

    Old
    Future Migration to pyQt5

--- src/old/main.py
+++ src/old/main.py
@@ -3,18 +3,19 @@ frontend
 '''
 import tkinter as tk
 from tkinter import ttk
-from windows.simulation_tab import Window
+from windows.simulation_tab import Window, Simulation
 
 # Main window definition
 main_window = tk.Tk()
 main_window.title("Robotat GUI")
-main_window.geometry("800x500")
-main_window.resizable(0, 0)
+main_window.geometry("800x600") # (x y)
+
+# main_window.resizable(0, 0)
 
 # Tabs definition
 notebook = ttk.Notebook(main_window)
 
-sim_tab = Window(notebook)
+sim_tab = Simulation(notebook)
 dictionary_sim = {
     "er1" : "Test",
     "er2" : "Test2"
@@ -29,29 +30,7 @@ notebook.add(ota_tab, text="OTA")
 rtd_tab = Window(notebook)
 notebook.add(rtd_tab, text="RTD")
 
-
+notebook.grid()
 notebook.pack(expand=True, fill="both")
 
 main_window.mainloop()
-
-
-  
-# lbl = Label(app,text = "A list of favourite countries...")  
-# lbl.pack() 
-# listbox = Listbox(app)  
-# listbox.pack()  
-
-
-# listbox.insert(1,"text 1")  
-  
-# listbox.insert(2, "text 2")  
-  
-# listbox.insert(3, "text 3")  
-  
-# listbox.insert(4, "text 4")  
-  
- 
-
-  
-# app.mainloop()  
-
--- src/old/simulation_tab.py
+++ src/old/simulation_tab.py
@@ -14,20 +14,33 @@ class Window(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
+        
+        # window geometry
+        # rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        # for i in rows:
+        #     self.grid_rowconfigure(i, minsize=60)
 
+        # self.grid_columnconfigure(0, weight=4, minsize=25)
+        # self.grid_columnconfigure(1, weight=1, minsize=25)
+        # self.grid_columnconfigure(2, weight=1, minsize=25)
+        # self.grid_columnconfigure(3, weight=1, minsize=25)
+        
         # close
         self.btn = ttk.Button(self, text="Close", command=self.close_window)
-        self.btn.pack(side=tk.TOP)
+        self.btn.pack()
+        # self.btn.grid(row=10, column=4)
 
         # date
         self.today_info = tk.StringVar()
         self.update_date()
         self.today_info_disp = ttk.Label(self, textvariable=self.today_info)
+        # self.today_info_disp.grid(row=0, column=2)
         self.today_info_disp.pack(side=tk.TOP)
 
         # console
         self.console = tk.Text(self, height=4)
-        self.console.pack(fill="x")
+        # self.console.grid(row=8, column=0)
+        self.console.pack(fill="x", side=BOTTOM)
         # initial message
         self.console.insert("end", "Robotat GUI console")
         # disable input option
@@ -38,6 +51,11 @@ class Window(ttk.Frame):
             "error1":"Error 1"
         }
 
+        # canvas for drawing
+        self.visualization = tk.Canvas(self, width=380, height=480, bg="white", highlightthickness=1, highlightbackground="black")
+        # self.visualization.grid(row = 1, column = 0, sticky=W)
+        self.visualization.pack(side=LEFT)
+        # self.grid()
         self.pack(expand=True, fill="both")
 
     def close_window(self):
@@ -56,3 +74,14 @@ class Window(ttk.Frame):
 
     def set_error_list(self, error_list):
         self.error_dictionary = error_list
+
+
+class Simulation(Window):
+    def increase_number():
+        None
+        
+    Window.no_robots = ttk.Spinbox(from_=0, command=increase_number, 
+                                   increment=1)
+    Window.no_robots.pack()
+
+    
