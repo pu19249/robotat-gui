bdf0a2cae96eafcb32919053e30925512aab404f
Jonathan Emanuel Pu Aguilera <pu19249@uvg.edu.gt>
2023-09-05 10:32:43 -0600

    get pose with python

--- src/tcp_connect.py
+++ src/tcp_connect.py
@@ -3,76 +3,82 @@ import sys
 import json
 import time
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def robotat_connect():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('192.168.50.200', 1883)
+    # Connect the socket to the port where the server is listening
+    server_address = ('192.168.50.200', 1883)
 
-print(sys.stderr, 'connecting to %s port %s' % server_address)
-sock.connect(server_address)
+    print(sys.stderr, 'connecting to %s port %s' % server_address)
+    sock.connect(server_address)
 
-try:
-    
-    # Send data
-    message = b'Example'
-    print(sys.stderr, 'sending "%s"' % message)
-    sock.sendall(message)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(sys.stderr, 'received "%s"' % data)
-
-finally:
-    print(sys.stderr, 'closing socket')
-    # sock.close()
-
-# time.sleep(1)
-# sock.connect(server_address)
-agents_ids = [1]
-timeout_count = 0
-timeout_in_100ms = 1 / 0.1
-mocap_data = None
-rotrep = 'quat'
-if 0 < min(agents_ids) <= 100:
-    s = {
-        "dst": 1,  # DST robotat
-        "cmd": 1,  # cmd get pose
-        "pld": round(agents_ids[0])
-    }
-    json_str = json.dumps(s)
-    byte_value = json_str.encode('utf-8')
-    
-    # byte_value = int(json_str, 16).to_bytes((len(json_str)+1)//2,byteorder='big')
+    # s = {
+    #     "dst": 1,  # DST robotat
+    #     "cmd": 1,  # cmd get pose
+    #     }
+    # json_str = json.dumps(s) # Python equivalent to MATLAB's write(tcp_obj...)
     # byte_value = json_str.encode('utf-8')
-    sock.sendall(byte_value)
+        
+    # sock.sendall(byte_value)
+        
+    # Receive and decode data from the server
+    # received_data = sock.recv(2048)  # Adjust buffer size as needed
+    # print(received_data) #connection verification
+
+        # Close the socket connection
+    # if sock.close() == True:
+    #     print('Connection closed')
+
+    return sock
+
+
+def get_pose(tcp_obj, agents_ids, rotrep):
+    timeout_count = 0
+    timeout_in100ms = 1 / 0.1
+    tcp_obj.recv(2048)
+    print(min(agents_ids))
     
-# Receive and decode data from the server
-    received_data = sock.recv(1024)  # Adjust buffer size as needed
-    # received_data = np.reshape(received_data, (7, len(agents_ids))).transpose
+    if ((min(agents_ids) > 0) and (max(agents_ids) <= 100)):
+        s = {
+            "dst": 1,  # DST robotat
+            "cmd": 1,  # cmd get pose
+            "pld": agents_ids
+        }
 
-    # mocap_data = json.loads(received_data.decode('utf-8'))
+        tcp_obj.send(json.dumps(s).encode())
+        
+        data_str = tcp_obj.recv(2048)
+        print(data_str)
+        mocap_data = json.loads(data_str)
 
-    # Close the socket connection
-    sock.close()
+        mocap_data = np.array(mocap_data)
 
-    print(received_data)
+        num_agents = len(agents_ids)
+        mocap_data = mocap_data.reshape(num_agents, 7)
 
+        if rotrep == 'quat':
+            pass
+        else:
+            try:
+                quat_columns = mocap_data[:, 3:]
+                euler_angles = R.from_quat(quat_columns).as_euler('xyz', degrees=True)
+                mocap_data[:, 3:] = np.rad2deg(euler_angles)
 
+                mocap_data = mocap_data[:, :-1]
+            except Exception as e:
+                raise ValueError('Invalid Euler angle sequence.') from e
+    else:
+        print('ERROR: Invalid ID(s).')
 
-# function tcp_obj = robotat_connect(ip)
-# %UNTITLED2 Summary of this function goes here
-# %   Detailed explanation goes here
-#     port = 1883;
-#     try
-#         tcp_obj = tcpclient(ip, port);
-#     catch
-#         disp('ERROR: Could not connect to Robotat server.');
-#     end
-# end
+    return mocap_data
+
+# Assuming you have a valid 'robotat_connect' function
+robotat = robotat_connect()
+print(type(robotat))
+pose1 = get_pose(robotat, [2], 'quat')
+
+print(pose1)
